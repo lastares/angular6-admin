ac3c2a32442b82c992b153943ab8fac9e225b383
songyaofeng <songyaofeng@kuaiqiangche.com>
2018-07-20 17:57:46 +0800

    [feat:]上线

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  SERVER_URL: `/`,
+  SERVER_URL: `/angular6-admin/dist/angular-admin`,
   production: false,
   useHash: true,
     imageHost: 'http://angular.songyaofeng.com'
